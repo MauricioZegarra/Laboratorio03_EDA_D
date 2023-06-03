0ae2ccb5c73cad9c2ba6075370be39e4f1f39329
Mauricio Eduardo Zegarra Puma <mzegarrapu@unsa.edu.pe>
2023-06-03 09:57:33 -0500

    Agregada la clase Node

---
+++ Ejercicio03/Node.java
@@ -0,0 +1,30 @@
+public class Node <T> {
+    T data;
+    Node<T> nextNode;
+
+    public Node(T data) {
+        this.data = data;
+        this.nextNode = null;
+    }
+
+    public Node(T data, Node<T> nextNode) {
+        this.data = data;
+        this.nextNode = nextNode;
+    }
+
+    public void setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNextNode() {
+        return this.nextNode;
+    }
+
+    public T getData() {
+        return data;
+    }
+}
