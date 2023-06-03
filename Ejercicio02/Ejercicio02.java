0c8f9aad449856fbbafef67044a14359434043d2
Gonzalo01703 <glaymem@unsa.edu.pe>
2023-06-03 00:11:38 -0500

    se hizo un ejemplo en el main para ver si funciona la pila

--- Ejercicio02/Ejercicio02.java
+++ Ejercicio02/Ejercicio02.java
@@ -1,9 +1,10 @@
+import java.util.*;
 public class Ejercicio02 {
 	public static void main(String[] args) {
-		Stack<String> pila = new Stack<String>();
+		Stack<String> pila = new Stack<>();
         
         try {
-
+            /* 
             pila.push("AndreCapi");
             System.out.println(pila+ "\tpeek: " + pila.peek());
             pila.push("GonzaloNoHaceNada");
@@ -24,7 +25,19 @@ public class Ejercicio02 {
             System.out.println(pila + "\tpeek: " + pila.peek());
 
 			System.out.println(pila.search("AndreCapi"));
-                
+               */
+              
+               pila.push("Elemento 1");
+               pila.push("Elemento 2");
+               pila.push("Elemento 3");
+               
+               System.out.println(pila);
+               System.out.println("");
+               System.out.println("El ultimo elemento: "+pila.peek());
+               System.out.println("Elemento eliminado"+pila.pop());
+               
+               System.out.println(pila);
+               
             
         }catch(Exception e) {
             System.out.println(e.getMessage());
