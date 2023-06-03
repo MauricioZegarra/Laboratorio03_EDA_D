e917d8269cb90473ab3f618b325df13b5e00bda8
andre98652 <132710386+andre98652@users.noreply.github.com>
2023-06-02 23:13:22 -0500

    creacion de funciones necesarias

--- Ejercicio01/ejercicio1.java
+++ Ejercicio01/ejercicio1.java
@@ -4,5 +4,46 @@ public class ejercicio1 {
         System.out.println("hola");
     }
 	
+    public static int equalStacks(Stack<Integer> h1, Stack<Integer> h2, Stack<Integer> h3) {
+        int sum1 = suma(h1);
+        int sum2 = suma(h2);
+        int sum3 = suma(h3);
+        //mientras las sumas sean diferentes se repite el ciclo
+        while (!comparar(sum1, sum2, sum3)) {
+            if (sum1 >= sum2 && sum1 >= sum3) {
+                if (h1.isEmpty()) {
+                    break; // No se pueden eliminar mas elementos 
+                }
+                sum1 -= h1.pop();
+            } else if (sum2 >= sum1 && sum2 >= sum3) {
+                if (h2.isEmpty()) {
+                    break; // No se pueden eliminar mas elemetnos
+                }
+                sum2 -= h2.pop();
+            } else {
+                if (h3.isEmpty()) {
+                    break; // No se pueden eliminar mas elemetnos
+                }
+                sum3 -= h3.pop();
+            }
+        }
 
+        if (comparar(sum1, sum2, sum3)) {
+            return sum1; // si al final las sumas son igual retorna
+        } else {
+            return 0; // No hay altura máxima posible para las tres pilas
+        }
+    }
+    //suma los elementos de la pila
+    public static int suma(Stack<Integer> pila) {
+        int sum = 0;
+        for (int altura : pila) {
+            sum += altura;
+        }
+        return sum;
+    }
+    //retorna true si las sumas son iguales
+    public static boolean comparar(int sum1, int sum2, int sum3) {
+        return sum1 == sum2 && sum2 == sum3;
+    }
 }
